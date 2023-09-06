2b74431ea961d1e4782a49cb472beda7475ac680
Lewis Kim <lewiskim517@gmail.com>
2023-09-05 23:08:37 -0400

    New book modal and form

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 let library = [];
+let modal = document.getElementById("modal")
+let isModelOpen = false;
+let newBookButton = document.getElementById("newBook");
+
+newBookButton.addEventListener("click", () => {
+    if (isModelOpen == false) { 
+
+        modal.setAttribute("class", "visible");
+        isModelOpen = true;
+    } else {
+        modal.setAttribute("class", "hidden")
+        isModelOpen=false;
+    }
+});
 
 function Book(title, author, pages, read) {
     this.title = title
@@ -10,10 +24,11 @@ function Book(title, author, pages, read) {
     }
 }
 
-Book.prototype.getBook = function() {
-    console.log(this.title);
+function addBook() {
+    let author = document.getElementById("author").value;
+    let title = document.getElementById("title").value;
+    let pages = document.getElementById("pages").value;
+    let read = document.getElementById("read").checked ? true : false;
+    let book = new Book(title, author, pages, read);
+    library.push(book);
 }
-
-const user = new Book("JK Rowling","Bob Kim","100",true);
-user.info();
-user.getBook();
