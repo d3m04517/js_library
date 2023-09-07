48d268f8b91cfea880823c100fad98d594b4a064
Lewis Kim <lewiskim517@gmail.com>
2023-09-06 22:25:03 -0400

    feat: Create and remove books
    Updated form to be able to create a new book. Added card details for each book and a remove button.

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let library = [];
 let modal = document.getElementById("modal")
 let isModelOpen = false;
 let newBookButton = document.getElementById("newBook");
+let createBookButton = document.getElementById("create-book");
+let main = document.getElementsByClassName('main')[0];
+
 
 newBookButton.addEventListener("click", () => {
     if (isModelOpen == false) { 
@@ -12,6 +15,13 @@ newBookButton.addEventListener("click", () => {
         modal.setAttribute("class", "hidden")
         isModelOpen=false;
     }
+});
+
+createBookButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    addBook();
+    modal.setAttribute("class", "hidden")
+
 });
 
 function Book(title, author, pages, read) {
@@ -31,4 +41,41 @@ function addBook() {
     let read = document.getElementById("read").checked ? true : false;
     let book = new Book(title, author, pages, read);
     library.push(book);
+    
+    createCard(book);
+}
+
+function createCard(book) {
+    let card = document.createElement('div');
+    card.classList = 'card';
+    main.appendChild(card);
+
+    let descriptionContainer = document.createElement('div');
+    descriptionContainer.classList = 'description';
+
+    let title = document.createElement('h4');
+    let desc = document.createElement('p');
+    title.textContent = book.title;
+    desc.innerHTML = "Author: " + book.author + "<br/>Pages: " + book.pages + "<br/> Read: " + book.read;
+
+    card.appendChild(descriptionContainer);
+    descriptionContainer.appendChild(title);
+    descriptionContainer.appendChild(desc);
+
+    let button = document.createElement('button');
+    button.setAttribute('data-bookid', library.length);
+    button.textContent = "Remove Book";
+    button.addEventListener("click", (event) => {
+        let index = event.target.getAttribute('data-bookid');
+        removeBook(index);
+    });
+    card.appendChild(button);
+}
+
+function removeBook(index) {
+    library.slice(index, 1);
+    let button = document.querySelector(`button[data-bookid="${index}"]`);
+    let card = button.parentElement;
+
+    main.removeChild(card);
 }
